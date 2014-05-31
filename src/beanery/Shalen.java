e78cbc75fec629ac72627f6ddf9d55464738ce0b
smalabon <shalen_malabon@informatics.edu.ph>
2014-05-31 15:56:16 +0800

    sample onglee

---
+++ src/beanery/Shalen.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beanery;
+
+/**
+ *
+ * @author manila
+ */
+public class Shalen {
+    public static void main(String[] args) {
+        System.out.println("sample ");
+    }
+    
+}
